dc3b57b32a2e1dcab7494b8ee585b2a3deb5aae5
George Oduor <37648586+GeorgeOduor@users.noreply.github.com>
2023-10-16 08:56:03 +0300

    all

--- cbs/settings.py
+++ cbs/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e_sly9k7@q+3*4qi*3-97x76g!+nlc9)1q5-q+!=#jdc0q3wvw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -72,7 +72,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cbs.wsgi.application'
+WSGI_APPLICATION = 'cbs.wsgi.app'
+
 
 
 # Database
